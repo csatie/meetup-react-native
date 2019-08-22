463e1fc96f23a8404fc47fbcb086346a98904652
Camila  Satie <camila@MacBook-Pro-de-Camila.local>
2019-08-22 16:40:56 -0300

    eslint and reactotron config

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import './config/ReactotronConfig';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function App() {
+  return <View />;
+}
